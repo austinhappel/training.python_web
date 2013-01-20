6c4bc2d930f29c878b1a2a0a210eb13520d3947f
Austin Happel <austin@austinhappel.com>
2013-01-19 16:58:34 -0800

    finished assignment 3

--- assignments/week02/lab/http_serve3.py
+++ assignments/week02/lab/http_serve3.py
@@ -21,29 +21,44 @@ s.bind((host, port))
 s.listen(backlog)
 
 
+# Parse the client's request.
 def parse_request(request):
     print 'got request: ' + request
+
+    response_codes = {
+        200: 'HTTP/1.1 200 OK\r\n',
+        405: 'HTTP/1.1 405 Method Not Allowed\r\nAllow: GET, HEAD\r\n',
+        400: 'HTTP/1.1 400 Bad Request\r\n'
+    }
+
+    header_date = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()) + '\r\n'
+
     lines = request.split('\r\n')
 
+    # Request type is in the first line.
     index_get = lines[0].find('GET')
     index_http = lines[0].find('HTTP')
 
-    if (index_get > -1 and index_http > -1):
-        print 'Client is requesting HTML'
-        request = lines[0][(index_get + 3):index_http]
-        return "<h1>your request is: " + request + "</h1>"
+    # Return the URL of the request if it's a HTTP GET request,
+    # but throw a ValueError if not.
+    if index_http > -1:
+        if index_get > -1:
+            print 'Client is requesting HTML'
+            request = lines[0][(index_get + 3):index_http]
+            return response_codes[200] + header_date + '\r\n' + str("<h1>your request is: " + request + "</h1>")
 
+        # Bullet 3 wants us to raise a ValueError, but instead we'll return a 405
+        # raise ValueError('The request (%s) is not an HTTP GET request.' % (lines[0]))
+        print 'Have HTTP request, but invalid method.'
+        return response_codes[405]
 
-def ok_response(body):
-    header = 'HTTP/1.1 200 OK\r\n'
-    h_date = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()) + '\r\n'
-    everything = header + h_date + '\r\n' + body
-    return everything
+    # If this isn't an HTTP request, send 400 error (bad request)
+    return response_codes[400]
+
 
 while True:  # keep looking for new connections forever
     client, address = s.accept()  # look for a connection
     data = client.recv(size)
     if data:  # if the connection was closed there would be no data
-        response = parse_request(data)
-        client.send(ok_response(response))
+        client.send(parse_request(data))
         client.close()

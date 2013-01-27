8472316c851b0787e2c92dcc91858f8ab5777136
Austin Happel <austin@austinhappel.com>
2013-01-26 22:41:22 -0800

    last.fm user and related artists searches working more or less

--- assignments/week03/athome/mixfindr/server.py
+++ assignments/week03/athome/mixfindr/server.py
@@ -1,6 +1,9 @@
 import os
 import logging
-
+import urllib2
+import urllib
+import json
+import types
 from pyramid.config import Configurator
 from wsgiref.simple_server import make_server
 
@@ -17,10 +20,13 @@ log.setLevel(logging.DEBUG)
 # working directory
 here = os.path.dirname(os.path.abspath(__file__))
 
+# last.fm base url
+last_fm_base_url = 'http://ws.audioscrobbler.com/2.0/'
+last_fm_api_key = str(os.environ.get('last_fm_api_key'))
+
 
 @view_config(route_name='index', renderer='templates/index.mustache')
 def index_view(request):
-    log.debug('index')
     data = {}
 
     return data
@@ -29,8 +35,24 @@ def index_view(request):
 @view_config(route_name='artist', renderer='templates/artist.mustache')
 def artist_view(request):
     artist_id = str(request.matchdict['id'])
+    url_similar_artists = '%s?method=artist.getsimilar&%s&format=json' % (last_fm_base_url, urllib.urlencode({'artist': artist_id, 'api_key': last_fm_api_key}))
+    req_similar_artists = urllib2.urlopen(url_similar_artists)
+    similar_artists = json.loads(req_similar_artists.read())
+
+    similar_artists_names = []
+    log.debug(similar_artists)
+    log.debug(type(similar_artists['similarartists']['artist']))
+    log.debug(isinstance(similar_artists['similarartists']['artist'], types.DictionaryType))
+
+    if similar_artists['similarartists'] != None and\
+    isinstance(similar_artists['similarartists']['artist'], types.ListType):
+        for x in similar_artists['similarartists']['artist'][0:5]:
+            similar_artists_names.append(x['name'])
+
     data = {
-        'artist': artist_id
+        'artist': artist_id,
+        'similar_artists_names': similar_artists_names,
+        'json': json.dumps(similar_artists['similarartists']['artist'][0:5], indent=4)
     }
 
     return data
@@ -39,8 +61,21 @@ def artist_view(request):
 @view_config(route_name='user', renderer='templates/user.mustache')
 def user_view(request):
     user_id = str(request.matchdict['id'])
+    url_user_top_artists = '%s?method=user.gettopartists&%s&format=json' % (last_fm_base_url, urllib.urlencode({'user': user_id, 'api_key': last_fm_api_key}))
+    req_user_top_artists = urllib2.urlopen(url_user_top_artists)
+    user_top_artists = json.loads(req_user_top_artists.read())
+    user_top_artists_names = []
+
+    for x in user_top_artists['topartists']['artist'][0:5]:
+        log.debug(x)
+        user_top_artists_names.append({
+            'name': x['name'],
+            'url': urllib2.quote(x['name'].encode('unicode-escape'))
+        })
     data = {
-        'user': user_id
+        'user': user_id,
+        'json': json.dumps(user_top_artists, indent=4),
+        'user_top_artists_names': user_top_artists_names
     }
     return data
 
